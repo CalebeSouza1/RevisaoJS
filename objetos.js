2e00c748e2066e89b3ad835ec0caa80aa0df02aa
CalebeSouza1 <calebesouzadeoliveira0@gmail.com>
2021-09-20 11:42:49 -0300

    segundo commit

---
+++ objetos.js
@@ -0,0 +1,15 @@
+const listaCPFs = ["12465565706", "65437895432", "15849385748"]
+
+// const cliente = ["nome", "André", "idade", 36]
+
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "12465565706",
+    email: "andre@email.com"
+}
+
+console.log(cliente.nome)
+console.log(cliente.idade)
+console.log(cliente.cpf)
+console.log(cliente.email)
